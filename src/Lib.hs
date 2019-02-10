199e5bcbe9ccb1e0703b67ddde4dbd10ad57435a
Alexandre Baldé <alexandrer_b@outlook.com>
2019-02-10 00:56:29 +0000

    Add simplest datatypes

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,27 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- | Datatype representing a transaction input. Per the instructions, it
+-- contains a numerical identifier and an amount. This datatype alone
+-- does not enforce the identifier's uniqueness.
+data TxIn = TxIn
+    { id     :: !Integer
+    , amount :: !Integer
+    } deriving (Show, Eq)
+
+-- | Datatype representing a transaction output. Per the instructions, it
+-- contains a numerical identifier and an amount. This datatype alone
+-- does not enforce the identifier's uniqueness.
+data TxOut = TxOut
+    { id     :: !Integer
+    , amount :: !Integer
+    } deriving (Show, Eq)
+
+-- | Datatype representing a transaction. This datatype's constructors or
+-- accessors alone do not enforce the requested property that the total
+-- amount present in the inputs match the total amount present in the outputs.
+data Transaction = Tx
+    { inputs  :: [TxIn]
+    , outputs :: [TxOut]
+    } deriving (Show, Eq)
