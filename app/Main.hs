22b3bc8dcce7868473713bbd0bad7a5f735f8353
Alexandre Baldé <alexandrer_b@outlook.com>
2019-02-15 02:03:09 +0000

    Refactor main to use an IORef
    This avoids recursively passing around the information regarding
    the blocktree. In this situation, it makes sense to use a global
    variable. Note that IORefs are NOT thread-safe.

--- app/Main.hs
+++ app/Main.hs
@@ -1,71 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
+module Main
+    ( main
+    ) where
 
 import Blockchain
-import Lib                                  ()
 
 import qualified Data.Aeson                 as A
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Lazy       as BSL
 import qualified Data.ByteString.Lazy.Char8 as BSLC
+import Data.IORef                           (IORef, newIORef, readIORef,
+                                             writeIORef)
+import System.IO.Unsafe                     (unsafePerformIO)
 
 main :: IO ()
-main = loop (BlockTree mempty mempty)
+main = loop
   where
+
+    -- The @NOINLINE@ is to avoid problems with @unsafePerformIO@.
+    -- Note that @IORef@s are, for the most part, NOT thread-safe.
+    ref :: IORef BlockTree
+    {-# NOINLINE ref #-}
+    ref = unsafePerformIO $ newIORef $ BlockTree mempty mempty
+
     output :: A.ToJSON a => a -> IO ()
     output = BSLC.putStrLn . A.encode
 
     -- Helper used to remove boilerplate when handling @Init@ or @Submit@
     -- commands - what to do if the command fails, and if it succeeds.
     handleResult :: (A.ToJSON out)
-                 => Either CommandException a
+                 => Either CommandException BlockTree
                  -- ^ @Either@ value to be pattern matched
-                 -> a
-                 -- ^ Value to perform @IO@ action with if above argument is
-                 -- @Left@
                  -> out
                  -- ^ Value to JSON-encode and write to @stdout@ if first
                  -- argument is @Right@.
-                 -> (a -> IO ())
-                 -- ^ @IO@ action to take after using @output@ to write to
-                 -- @stdout@.
                  -> IO ()
-    handleResult res left out go = case res of
+    handleResult res out = case res of
         Left err -> do output err
-                       go left
+                       loop
         Right r  -> do output out
-                       go r
+                       writeIORef ref r
+                       loop
 
     -- This helper is like @handleResult@, but for the
     -- @QueryState@/@QueryHeads@ commands. @handleResult@ cannot be used
     --  because these commands do not use the value inside @Right@ to @loop@;
     -- in this case the client just reuses the data it had before to await new
     -- commands.
-    handleResult' :: (A.ToJSON out)
-                  => Either CommandException a -> IO () -> IO ()
-    handleResult' res go = case res of
+    handleResult' :: A.ToJSON a => Either CommandException a -> IO ()
+    handleResult' res = case res of
         Left err -> do output err
-                       go
+                       loop
         Right query -> do output query
-                          go
+                          loop
 
-    -- Main worker in the client. To note that a @BlockTree@ can only be
+    -- Main worker in the client. Note that a @BlockTree@ can only be
     -- initialized once - if it happens more than once, the binary prints
     -- that it has already been initialized, and awaits another command.
-    loop :: BlockTree -> IO ()
-    loop m = do
+    loop :: IO ()
+    loop = do
         -- As mentioned in the spec, the client expects newline-delimited JSON.
         -- It will not work otherwise.
         s <- BS.getLine
         case A.eitherDecode (BSL.fromStrict s) of
             Left err -> do putStrLn err
-                           loop m
-            Right command -> case command of
-                Init b -> if isEmpty m
-                    then handleResult (initTree b) m OK loop
-                    else do BSLC.putStrLn "Chain has already been initialized"
-                            loop m
-                QueryState -> handleResult' (querySt m) (loop m)
-                QueryHeads -> handleResult' (queryHd m) (loop m)
-                Submit b -> handleResult (submitBlock b m) m OK loop
+                           loop
+            Right command -> do
+                blocktree <- readIORef ref
+                case command of
+                    Init b -> if isEmpty blocktree
+                        then handleResult (initTree b) OK
+                        else do BSLC.putStrLn "Chain has already been initialized"
+                                loop
+                    QueryState -> handleResult' (querySt blocktree)
+                    QueryHeads -> handleResult' (queryHd blocktree)
+                    Submit b -> handleResult (submitBlock b blocktree) OK
